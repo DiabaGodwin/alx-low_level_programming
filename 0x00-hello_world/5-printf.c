c408e3dedadfedf4c3e893be3d6082f9b7233b68
Godwin Diaba <93232970+DiabaGodwin@users.noreply.github.com>
2022-04-08 05:10:32 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
 *main : entry point
-*Printf task
+*My hello, printf task
+*Return: return one after the program execute
 */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a peice of art,\n");
+printf("with proper grammer, but the outcome is a piece of art,\n");
   return (0);
 }
