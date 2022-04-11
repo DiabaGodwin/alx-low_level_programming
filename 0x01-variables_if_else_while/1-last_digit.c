62f6bf6a7a33be5effac46d5e0a7edb21ad669ac
Godwin Diaba <93232970+DiabaGodwin@users.noreply.github.com>
2022-04-11 08:31:11 +0000

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,25 +5,27 @@
 /**
 *main - Enrty point
 *
-*Return: Always 0 (Successs/correct)
+*Return: Always 0 (Success/correct)
 */
 
 int main(void)
+
 {
 int n;
 srand(time(0));
-n = read() - RAND_MAX / 2;
-if (n == 0)
+n = rand() - RAND_MAX / 2;
+
+if (n % 10 > 5)
 {
-printf("%i is zero\n", n);
+printf("last digit of %i is %i and greater than \n", n, n % 10);
 }
-else if (n < 0)
+else if (n % 10 == 0)
 {
-printf("%i is negative\n", n);
+printf("Last digit of %i is %i and is 0\n", n, n % 10) 
 }
 else if (n > 0)
 {
-printf("%i is positive\n", n);
+printf("%i is positive\n", n n % 10);
 }
 return (0);
 }
