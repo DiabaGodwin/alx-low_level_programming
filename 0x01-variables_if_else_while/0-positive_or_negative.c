232b798a59365423d601238d118aa2d663ecfa2d
Godwin Diaba <93232970+DiabaGodwin@users.noreply.github.com>
2022-04-11 07:32:54 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,17 +13,18 @@ int main(void)
 int n;	
 srand(time(0));
 n = read() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%i is positive\n", n);
-}
-else if (n == 0)
+  
+if (n == 0)
 {
 printf("%i is zero\n", n);
 }
 else if (n < 0)
 {
 printf("%i is negative\n", n);
+}
+else if (n > 0)
+{
+printf("%i is positive\n", n);
 }
 return (0);
 }
