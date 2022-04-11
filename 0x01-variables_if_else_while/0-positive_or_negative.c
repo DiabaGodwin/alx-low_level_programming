842d80f74e6b32e8e38c72c57c1015a61b9bee61
Godwin Diaba <93232970+DiabaGodwin@users.noreply.github.com>
2022-04-11 07:22:06 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
 * main - Enrty point
 *
 * Return: Always 0 (Success/correct)
 */
+
 int main(void)
 {
 int n;	
 srand(time(0));
-n = read() -RAND_MAX /2;
-if (n>0)
+n = read() -RAND_MAX / 2;
+if (n > 0)
 {
 printf("%i is positive\n", n);
 }
-else if (n==0)
+else if (n == 0)
 {
 printf("%i is zero\n", n);
 }
-else if (n<0)
+else if (n < 0)
 {
 printf("%i is negative\n", n);
 }
