b7ca70d8c4173ac1f2c97a1e39cb2a67da33a35c
Godwin Diaba <93232970+DiabaGodwin@users.noreply.github.com>
2022-04-14 17:50:57 -0700

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* mul -multiplies two integers.
+* mul -multiplies two integers
 *@a: print int
 *@b: print int
 * Return: Always 0.
@@ -8,5 +8,5 @@
 
 int mul(int a, int b)
 {
-*return (a * b);
+return (a * b);
 }
